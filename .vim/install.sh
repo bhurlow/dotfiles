d2269dc98f06c3526f7bb1022572d0454041d999
Brian Hurlow <brian@brianhurlow.com>
2021-02-07 18:11:41 -0500

    vimrc updates for clojure-lsp

---
+++ .vim/install.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+mkdir ~/.vim/bundle
+pushd ~/.vim/bundle
+
+packages=(
+ ctrlpvim/ctrlp.vim
+ easymotion/vim-easymotion
+ ervandew/supertab
+ guns/vim-clojure-static
+ dag/vim-fish
+ tpope/vim-repeat
+ tpope/vim-surround
+ mattn/emmet-vim
+ godlygeek/tabular
+ tpope/vim-commentary
+ tpope/vim-fireplace
+ bhurlow/vim-parinfer
+ tpope/vim-unimpaired
+ tpope/vim-speeddating
+ fatih/vim-go
+ pangloss/vim-javascript
+ prettier/vim-prettier
+ w0rp/ale
+ mxw/vim-jsx
+ ElmCast/elm-vim
+ neoclide/coc.nvim
+)
+
+for x in "${packages[@]}"
+do
+  echo $var
+  git clone https://github.com/$x
+done
